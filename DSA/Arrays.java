3c9a41d6cd1b53ed9e9f8612da709c78de315170
Soumyaranjan Behera <soumya.rbehera007@gmail.com>
2025-10-25 23:01:53 +0530

    Max element finding Method

--- DSA/Arrays.java
+++ DSA/Arrays.java
@@ -1,7 +1,29 @@
 package DSA;
 
+//import java.util.Scanner;
+
 public class Arrays {
+//    static
+    public static int maxFromArray(int[] arr){
+        int large=0;
+    for(int i=0;i<arr.length;i++){
+            if(large<arr[i]){
+                large =arr[i];
+            }
+        }
+//        System.out.println("hello");
+    return large;
+    }
     public static void main(String[] args) {
+
+//    creating an array of size n
+//        Scanner sc= new Scanner(System.in);
+//        int n=sc.nextInt();
+//    int arr1[] =new int[n];
+    int arr[] ={122,1,3,5,4,3,23};
+    int large =maxFromArray(arr);
+        System.out.println(large);
+
 
     }
 }
--- DSA/HelloWorldTesting.java
+++ DSA/HelloWorldTesting.java
@@ -1,7 +1,7 @@
 package DSA;
-
 public class HelloWorldTesting {
     public static void main(String[] args) {
         System.out.println("Hello World to GitHub");
+
     }
 }
